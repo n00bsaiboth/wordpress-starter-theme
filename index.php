c2e1f1a60b5fec009f541be6d53ddff5f8bac6b0
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2021-12-06 15:04:06 +0200

    Updated the index.php file

--- index.php
+++ index.php
@@ -9,10 +9,13 @@ get_header();
 
 <?php if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php the_title(); ?>
-
+<article id="post-<?php the_ID(); ?> <?php post_class(); ?>>
+<div>
+<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+</div>
+<div>
 <?php the_content(); ?>
-
+</div>
 <?php endwhile; ?>
 
 <?php else: ?>
